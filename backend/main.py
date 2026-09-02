4416ac206eae09d92ff7798dc90d1533120a5a04
parchedAlbumen <kennethpamintuan2305@gmail.com>
2026-09-01 20:52:26 -0700

    start the backend with 2 endpoints, almost done with lesson 2

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "hello everyone!"}
+
+@app.get("/runs")
+def list_runs():
+    return [
+        {"id": 1, "date": "2026-08-25", "distance_km": 5.0, "duration_min": 27.5},
+        {"id": 2, "date": "2026-08-26", "distance_km": 6.0, "duration_min": 30}
+    ]
